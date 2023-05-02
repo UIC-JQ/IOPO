9651ed453984c262b136e03c3ac1f4cad3c389f7
Zhongyi <zhongyicst@gmail.com>
2023-05-02 20:58:50 +0800

    5/2 refine test_compare_diff_method

--- dataclass.py
+++ dataclass.py
@@ -1,12 +1,26 @@
 import numpy as np
+import csv
+from tqdm import tqdm
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 class DataConfig:
     def __init__(self, 
                  n_of_uav=None,
                  n_of_user=None,
-                 penalty=1000) -> None:
-        assert n_of_user is not None and n_of_user is not None
+                 penalty=1000,
+                 load_config_from_path=None) -> None:
+        
+        if load_config_from_path is not None:
+            self.load_config_from_file(load_config_from_path)
+            return
 
+        assert n_of_user is not None and n_of_user is not None
         # CONSTANT VALUES:
         self.SPEED_OF_LIGHT                 = 299792458        # speed of the light m/s
         self.TIME_SLOT_LENGTH               = 0.125 * 1e-3     # s equal to 0.125ms,time of one slot
@@ -72,6 +86,91 @@ class DataConfig:
             raise NotImplementedError()
         
         self.uav_coordinate = __uav_coordinate[self.uav_number]
+
+
+        # 用于生成数据集的设置
+        self.board_x_size = 600
+        self.board_y_size = 800
+
         
-        # debug:
-        # print(self.__dict__)
+    def generate_dataset(self, n=5000, saving_path=None):
+        la = [] #训练数据
+
+        # features:
+        for _ in tqdm(range(n)):
+            lm = []
+
+            # 生成用户坐标：(x, y, z)
+            for _ in range(self.user_number):
+                lm.append(np.random.uniform(0, self.board_x_size))
+                lm.append(np.random.uniform(0, self.board_y_size))
+                lm.append(0)
+
+            # 生成task info   
+            for _ in range(self.user_number):
+                data = round(np.random.uniform(3e2, 5e2)) #用户平均任务大小 Mbit/slot单位时间多少Mbit
+                lm.append(data)
+
+                cpb = np.random.randint(40,150)*1e-3 # 单位比特计算次数为 40-150 cycles
+                data = np.round(data * cpb)#任务需要的cpu数量
+                lm.append(data)
+
+                lm.append(np.random.randint(np.ceil(data*1e6/125000),np.ceil(data*1e6/125000)+1000))#任务可以接受的延迟 slot
+
+            la.append(lm)
+
+        self.__save_to_csv(la, saving_path)
+        
+    
+    def __save_to_csv(self, data, file_path):
+        with open(file_path, mode='w+', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+
+    def load_config_from_file(self, file_path):
+        """
+        作用：从配置文件file_path中读取配置
+        """
+
+        print('---> loading config from file: "{}"'.format(file_path))
+
+        with open(file_path, 'r') as f:
+            for k, v in json.load(f).items():
+                # 将所有list转换为ndarray类型
+                if type(v) == list:
+                    self.__dict__[k] = np.array(v)
+                    continue
+
+                self.__dict__[k] = v
+
+        print('Done.')
+        print('Loaded configs:')
+        for k, v in self.__dict__.items():
+            print('* name: {}, value: {}, type: {}'.format(k, v, type(v)))
+        
+        
+    def save_config(self, file_path):
+        """
+        作用：将本次使用的配置保存到文件file_path中
+        """
+
+        print('---> saving configs to file: "{}"'.format(file_path))
+
+        with open(file_path, 'w', encoding='utf-8') as fp:
+            json.dump(self.__dict__, fp, cls=NumpyEncoder)
+
+        print('Done.')
+        print('Saved configs:')
+        for k, v in self.__dict__.items():
+            print('* name: {}, value: {}, type: {}'.format(k, v, type(v)))
+
+if __name__ == '__main__':
+    # 创建对象
+    dataObj = DataConfig(load_config_from_path='config.json')
+    # dataObj = DataConfig(n_of_user=10, n_of_uav=10)
+
+    # 保存config
+    # dataObj.save_config('config.json')
+    
+    # 生成数据集:
+    dataObj.generate_dataset(n=1000, saving_path='./h.csv')
--- test_compare_diff_methods.py
+++ test_compare_diff_methods.py
@@ -4,7 +4,7 @@ from dataclass import DataConfig
 from opt3 import whale
 from tqdm import tqdm
 
-def try_method(method, method_name, **kws):
+def try_method(X, method, method_name, **kws):
     avg_eng_cost, overtime_records_ratio, avg_overtime_ratio, avg_overtime_people = method(X, data_config, **kws)
     print('--> 方法名: [%s]' % method_name)
     print('平均每条record的energy cost:', avg_eng_cost)
@@ -12,116 +12,79 @@ def try_method(method, method_name, **kws):
     print('所有存在超时user的record中，平均超时人数占总人数的比例: {}%'.format(avg_overtime_ratio * 100))
     print('所有存在超时user的record中，平均超时人数为', avg_overtime_people)
 
-# -------------------------------------------------------------
-# 比较方法1:全local
-def cmp_method_all_local_compute(X, data_config: DataConfig, **kws):
-    total_eng_cost = 0
-    total_overtime_records_num = 0
-    store_overtime_ratio = []
-    store_overtime_people = []
+def allocate_plan_all_local(record, data_config, **kws):
+    return np.zeros(data_config.user_number * data_config.uav_number)
 
-    __allocate_plan = np.zeros(X[0].shape)
-    for record in tqdm(X):
-        _, energy, overtime_logs = whale(record, __allocate_plan, data_config, need_stats=True)
+def allocate_plan_all_upload_random(record, data_config, **kws):
+    _, __allocate_plan = random_sample_lower_eng_cost_plan(record,
+                                                           data_config,
+                                                           K=kws['K'],
+                                                           exclude_local_options=True)
 
-        if overtime_logs:
-        # 统计超时数据stat
-        # 如果这条record有超时
-            total_overtime_records_num += 1
-            store_overtime_ratio.append(len(overtime_logs) / data_config.user_number)
-            store_overtime_people.append(len(overtime_logs))
+    return __allocate_plan
 
-        total_eng_cost += energy
+def allocate_plan_local_and_upload_random(record, data_config, **kws):
+    _, __allocate_plan = random_sample_lower_eng_cost_plan(record,
+                                                           data_config,
+                                                           K=kws['K'],
+                                                           exclude_local_options=False)
+    return __allocate_plan
+
+
+def compare_method(f):
+    def inner(X, data_config, **kws):
+        total_eng_cost = 0
+        total_overtime_records_num = 0
+        store_overtime_ratio = []
+        store_overtime_people = []
+
+        for record in tqdm(X):
+            # 用给定方法生成一个 allocate plan
+            __allocate_plan = f(record, data_config, **kws)
+            # 计算energy cost
+            _, energy, overtime_logs = whale(record, __allocate_plan, data_config, need_stats=True)
+
+            if overtime_logs:
+            # 统计超时数据stat
+            # 如果这条record有超时
+                total_overtime_records_num += 1
+                store_overtime_ratio.append(len(overtime_logs) / data_config.user_number)
+                store_overtime_people.append(len(overtime_logs))
+
+            total_eng_cost += energy
+
+        # 计算stats: 
+        avg_eng_cost                    = total_eng_cost / len(X)
+        overtime_records_ratio          = total_overtime_records_num / len(X)
+        avg_overtime_ratio              = sum(store_overtime_ratio) / len(store_overtime_ratio) if len(store_overtime_ratio) else 0
+        avg_overtime_people             = sum(store_overtime_people) / len(store_overtime_people) if len(store_overtime_people) else 0
+
+        return avg_eng_cost, overtime_records_ratio, avg_overtime_ratio, avg_overtime_people
     
-    avg_eng_cost                    = total_eng_cost / len(X)
-    overtime_records_ratio          = total_overtime_records_num / len(X)
-    avg_overtime_ratio              = sum(store_overtime_ratio) / len(store_overtime_ratio)
-    avg_overtime_people             = sum(store_overtime_people) / len(store_overtime_people)
+    return inner
 
-    return avg_eng_cost, overtime_records_ratio, avg_overtime_ratio, avg_overtime_people
-
-# -------------------------------------------------------------
-# 比较方法2: optimized random
-def cmp_method_local_and_upload_random(X, data_config: DataConfig, **kws):
-    total_eng_cost = 0
-    total_overtime_records_num = 0
-    store_overtime_ratio = []
-    store_overtime_people = []
-
-    for record in tqdm(X):
-        _, __allocate_plan = random_sample_lower_eng_cost_plan(record, data_config.user_number, data_config.uav_number, K=kws['K'])
-        _, energy, overtime_logs = whale(record, __allocate_plan, data_config, need_stats=True)
-
-        if overtime_logs:
-        # 统计超时数据stat
-        # 如果这条record有超时
-            total_overtime_records_num += 1
-            store_overtime_ratio.append(len(overtime_logs) / data_config.user_number)
-            store_overtime_people.append(len(overtime_logs))
-
-        total_eng_cost += energy
-    
-    avg_eng_cost                    = total_eng_cost / len(X)
-    overtime_records_ratio          = total_overtime_records_num / len(X)
-    avg_overtime_ratio              = sum(store_overtime_ratio) / len(store_overtime_ratio)
-    avg_overtime_people             = sum(store_overtime_people) / len(store_overtime_people)
-
-    return avg_eng_cost, overtime_records_ratio, avg_overtime_ratio, avg_overtime_people
-
-# -------------------------------------------------------------
-# 比较方法3: optimized random (all upload)
-def cmp_method_all_upload_random(X, data_config: DataConfig, **kws):
-    total_eng_cost = 0
-    total_overtime_records_num = 0
-    store_overtime_ratio = []
-    store_overtime_people = []
-
-    for record in tqdm(X):
-        _, __allocate_plan = random_sample_lower_eng_cost_plan(record, data_config.user_number, data_config.uav_number,
-                                                                       K=kws['K'], exclude_local_options=True)
-        assert(sum(__allocate_plan) == data_config.user_number)
-        _, energy, overtime_logs = whale(record, __allocate_plan, data_config, need_stats=True)
-
-        if overtime_logs:
-        # 统计超时数据stat
-        # 如果这条record有超时
-            total_overtime_records_num += 1
-            store_overtime_ratio.append(len(overtime_logs) / data_config.user_number)
-            store_overtime_people.append(len(overtime_logs))
-
-        total_eng_cost += energy
-    
-    avg_eng_cost                    = total_eng_cost / len(X)
-    overtime_records_ratio          = total_overtime_records_num / len(X)
-    avg_overtime_ratio              = sum(store_overtime_ratio) / len(store_overtime_ratio)
-    avg_overtime_people             = sum(store_overtime_people) / len(store_overtime_people)
-
-    return avg_eng_cost, overtime_records_ratio, avg_overtime_ratio, avg_overtime_people
-
-def random_sample_lower_eng_cost_plan(record, user, uav, K=100, exclude_local_options=False):
+def random_sample_lower_eng_cost_plan(record, data_config, K=100, exclude_local_options=False):
     """
     date: 2023/4/30
     author: Yu, Jianqiu
     """
-    config_sol_size = user * uav
+    config_sol_size = data_config.user_number * data_config.uav_number
     config_random_times = K
         
     e_best = float('inf')
     lower_b = 0 if not exclude_local_options else 1
-    higher_b = uav + 1
+    higher_b = data_config.uav_number + 1
 
     for _ in range(config_random_times):
         random_sol = np.zeros(config_sol_size, dtype=int)
         
-        for i in range(0, config_sol_size, uav):
+        for i in range(0, config_sol_size, data_config.uav_number):
             idx = np.random.randint(lower_b, higher_b)
             if idx == 0:
                 continue
             random_sol[i + idx - 1] = 1
         
-        # print('random solution:', random_sol)
-
-        _, new_energy = whale(record, random_sol, data_config)#重新算结果
+        _, new_energy = whale(record, random_sol, data_config) # 重新算结果
 
         if new_energy < e_best:
             e_best = new_energy
@@ -138,7 +101,7 @@ if __name__ == '__main__':
     # LOAD Test data:
     path = './test_data/testData_userNumber=%d_n=5000.csv' % n_of_users
 
-    n = 50
+    n = 10
     with open(path, mode='r') as file:
         # 创建CSV读取器，指定分隔符为逗号
         reader = csv.reader(file, delimiter=',')
@@ -150,7 +113,7 @@ if __name__ == '__main__':
             n -= 1
         X = np.array(X, dtype=float)
     
-    try_method(cmp_method_all_upload_random, 'ALL UPLOAD RANDOM (K=1)', K=1)
-    try_method(cmp_method_all_upload_random, 'ALL UPLOAD RANDOM (K=10)', K=10)
-    try_method(cmp_method_local_and_upload_random,  'BOTH LOCAL AND UPLOAD RANDOM (K=10)', K=10)
-    try_method(cmp_method_all_local_compute, 'ALL LOCAL COMPUTE')
+    try_method(X, compare_method(allocate_plan_all_upload_random), 'ALL UPLOAD RANDOM (K=1)', K=1)
+    try_method(X, compare_method(allocate_plan_all_upload_random), 'ALL UPLOAD RANDOM (K=10)', K=10)
+    try_method(X, compare_method(allocate_plan_local_and_upload_random), 'BOTH LOCAL AND UPLOAD RANDOM (K=10)', K=10)
+    try_method(X, compare_method(allocate_plan_all_local), 'ALL LOCAL', K=1)
